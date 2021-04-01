bfe24895d98268a5b4308b89efcb6d98a12cd3ab
Ethan <ethan.parker13@gmail.com>
2021-04-01 12:20:16 +0100

    April 1st

--- CollageCS/Program.cs
+++ CollageCS/Program.cs
@@ -10,15 +10,75 @@ namespace CollageCS
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Give me two numbers between 0 - 255");
-            Console.WriteLine("Give me the first number: ");
+            Console.Write("Give me the first number: ");
             byte bNum1 = Convert.ToByte (Console.ReadLine());
-            Console.WriteLine("Give me the second number: ");
+            Console.Write("Give me the second number: ");
             byte bNum2 = Convert.ToByte (Console.ReadLine());
             Console.WriteLine(bNum1);
             Console.WriteLine(bNum2);
+            */
 
             Console.ReadKey();
+
+        }
+        public void Rocket()
+        {
+            Console.WriteLine("\n\n\n\n\n\n\n  *\n ***\n*****\n*****\n*****\n*****\n*****\n ***\n  *\n ***\n*****");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("\n\n\n\n\n\n  *\n ***\n*****\n*****\n*****\n*****\n*****\n ***\n  *\n ***\n*****");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("\n\n\n\n\n  *\n ***\n*****\n*****\n*****\n*****\n*****\n ***\n  *\n ***\n*****");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("\n\n\n\n  *\n ***\n*****\n*****\n*****\n*****\n*****\n ***\n  *\n ***\n*****");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("\n\n\n  *\n ***\n*****\n*****\n*****\n*****\n*****\n ***\n  *\n ***\n*****");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("\n\n  *\n ***\n*****\n*****\n*****\n*****\n*****\n ***\n  *\n ***\n*****");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("\n  *\n ***\n*****\n*****\n*****\n*****\n*****\n ***\n  *\n ***\n*****");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("  *\n ***\n*****\n*****\n*****\n*****\n*****\n ***\n  *\n ***\n*****");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine(" ***\n*****\n*****\n*****\n*****\n*****\n ***\n  *\n ***\n*****");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("*****\n*****\n*****\n*****\n*****\n ***\n  *\n ***\n*****");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("*****\n*****\n*****\n*****\n ***\n  *\n ***\n*****");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("*****\n*****\n*****\n ***\n  *\n ***\n*****");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("*****\n*****\n ***\n  *\n ***\n*****");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("*****\n ***\n  *\n ***\n*****");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine(" ***\n  *\n ***\n*****");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("  *\n ***\n*****");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine(" ***\n*****");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("*****");
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
         }
     }
 }
