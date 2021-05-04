ee31100e0d0e3c8db6ce6f18b6da17001b36783a
Ethan P <ejparker1@live.brock.ac.uk>
2021-05-04 10:09:22 +0100

    Added return to menu after running a project
    Added fundamentals for upcoming dev mode
    Tweaked Selection project to catch errors

--- CollageCS/Program.cs
+++ CollageCS/Program.cs
@@ -6,63 +6,88 @@ namespace CollageCS
     internal static class Program
     {
         private static int i;
+        private static int iSal;
 
         private static void Main()
         {
-            Console.Write("1 Projects\n2 Fun: ");
-            var bSel = Convert.ToByte(Console.ReadLine());
-            switch (bSel)
+            bool Menu = true;
+            while (Menu == true)
             {
-                case 1:
-                    Console.Write("\n\n1 Print Two Numbers\n2 Selection\n3 Switch\n4 Add two numbers\n5 Sub two numbers: ");
-                    var bSel2 = Convert.ToByte(Console.ReadLine());
-                    switch (bSel2)
-                    {
-                        case 1:
-                            PrintTwoNum();
-                            break;
-                        case 2:
-                            Selection();
-                            break;
-                        case 3:
-                            Switchtest();
-                            break;
-                        case 4:
-                            Console.Clear();
-                            int iNum1 = 9;
-                            int iNum2 = 10;
-                            AddTwoNumbers(ref iNum1, ref iNum2);
-                            break;
-                        case 5:
-                            Console.Clear();
-                            int iNum3 = 10;
-                            int iNum4 = 9;
-                            SubTwoNumbers(ref iNum3, ref iNum4);
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case 2:
-                    Console.Write("1 Rocket\n2 Car: ");
-                    var bSel3 = Convert.ToByte(Console.ReadLine());
-                    switch (bSel3)
-                    {
-                        case 1:
-                            Rocket();
-                            break;
-                        case 2:
-                            Car();
-                            break;
-                        default:
-                            Console.WriteLine("Something went wrong, try again with a number between 1-2");
-                            break;
-                    }
-                    break;
-                default:
-                    Console.WriteLine("Hmm, that's not right");
-                    break;
+                Console.Write("1 Projects\n2 Fun: ");
+                var bSel = Convert.ToByte(Console.ReadLine());
+                //byte bSel = 1;
+                //byte bSel2 = 1;
+                ////return true
+                switch (bSel)
+                {
+                    case 1:
+                        Console.Write("\n\n1 Print Two Numbers\n2 Selection\n3 Switch\n4 Add two numbers\n5 Sub two numbers: ");
+                        var bSel2 = Convert.ToByte(Console.ReadLine());
+                        switch (bSel2)
+                        {
+                            case 1:
+                                PrintTwoNum();
+                                break;
+                            case 2:
+                                Selection();
+                                break;
+                            case 3:
+                                Switchtest();
+                                break;
+                            case 4:
+                                Console.Clear();
+                                int iNum1 = 9;
+                                int iNum2 = 10;
+                                AddTwoNumbers(ref iNum1, ref iNum2);
+                                break;
+                            case 5:
+                                Console.Clear();
+                                int iNum3 = 10;
+                                int iNum4 = 9;
+                                SubTwoNumbers(ref iNum3, ref iNum4);
+                                break;
+                            default:
+                                break;
+                        }
+                        string sSel1;
+                        Console.Write("Would you like to run another project? y/n: ");
+                        sSel1 = Console.ReadLine();
+                        if (sSel1 == "y")
+                        {
+                            Menu = true;
+                        }
+                        else if(sSel1 == "n")
+                        {
+                            Menu = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry I was looking for a y/n...");
+                            Menu = false;
+                        }
+                        break;
+                    case 2:
+                        Console.Write("1 Rocket\n2 Car: ");
+                        var bSel3 = Convert.ToByte(Console.ReadLine());
+                        switch (bSel3)
+                        {
+                            case 1:
+                                Rocket();
+                                break;
+                            case 2:
+                                Car();
+                                break;
+                            default:
+                                Console.WriteLine("Something went wrong, try again with a number between 1-2");
+                                break;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Hmm, that's not right");
+                        break;
+                }
             }
+            
             Console.Write("Press any key to exit: ");
             Console.ReadKey();
         }
@@ -84,6 +109,7 @@ namespace CollageCS
 
         private static void PrintTwoNum()
         {
+            Console.Clear();
             Console.WriteLine("Give me two numbers between 0 - 255");
             Console.Write("Give me the first number: ");
             byte bNum1 = Convert.ToByte(Console.ReadLine());
@@ -125,8 +151,20 @@ namespace CollageCS
             //    Console.WriteLine("cold");
             //}
 
-            Console.Write("How much are you earning: £");
-            uint iSal = Convert.ToUInt16(Console.ReadLine());
+            
+            try
+            {
+                Console.Write("How much are you earning: £");
+                var iSal = Convert.ToUInt16(Console.ReadLine());
+            }
+            catch (System.OverflowException)
+            {
+                Console.WriteLine("Whoa there pal, that's a lot of money, why don't you try being a litlle more realistic...");
+                Console.Write("How much are you earning: £");
+                var iSal = Convert.ToUInt16(Console.ReadLine());
+                //throw;
+            }
+            
             Console.Write("and how long have you worked here: ");
             Byte bYears = Convert.ToByte(Console.ReadLine());
             if (iSal > 40000)
