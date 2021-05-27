654180bbf2cbcc46adadc833fde92ea1a8541f32
Ethan P <ejparker1@live.brock.ac.uk>
2021-05-27 11:30:23 +0100

    Taking out the trash
    Just important bug fixes. Nothing much.

--- CollageCS/Program.cs
+++ CollageCS/Program.cs
@@ -7,14 +7,12 @@ namespace CollageCS
         private static int i;
         private static void Main()
         {
-            bool DevMode = true;
+            bool DevMode = false;
             bool Menu = true;
             if (DevMode == true)
             {
                 Menu = false;
                 Console.ForegroundColor = ConsoleColor.Green;
-                //Console.BackgroundColor = System.Drawing.ColorTranslator.FromHtml("#F9DA00");
-                //Console.Clear();
                 AnArry();
             }
 
@@ -26,7 +24,7 @@ namespace CollageCS
                 switch (bSel)
                 {
                     case 1:
-                        Console.Write("\n\n1 Print Two Numbers\n2 Selection\n3 Switch\n4 Add two numbers\n5 Sub two numbers\n6 Interest Calculator: ");
+                        Console.Write("\n1 Print Two Numbers\n2 Selection\n3 Using Switch State's\n4 Add two numbers\n5 Sub two numbers\n6 Interest Calculator\n7 Student Grade Calc: ");
                         var bSel2 = Convert.ToByte(Console.ReadLine());
                         switch (bSel2)
                         {
@@ -45,15 +43,19 @@ namespace CollageCS
                             case 4:
                                 //Add Two Numbers - 29/04/21
                                 Console.Clear();
-                                int iNum1 = 9;
-                                int iNum2 = 10;
+                                Console.Write("Enter first number you'd like to add: ");
+                                int iNum1 = Convert.ToInt16(Console.ReadLine());
+                                Console.Write("and the second: ");
+                                int iNum2 = Convert.ToInt16(Console.ReadLine());
                                 AddTwoNumbers(ref iNum1, ref iNum2);
                                 break;
                             case 5:
                                 //Sub Two Numbers - 29/04/21
                                 Console.Clear();
-                                int iNum3 = 10;
-                                int iNum4 = 9;
+                                Console.Write("Enter first number you'd like to add: ");
+                                int iNum3 = Convert.ToInt16(Console.ReadLine());
+                                Console.Write("and the second: ");
+                                int iNum4 = Convert.ToInt16(Console.ReadLine());
                                 SubTwoNumbers(ref iNum3, ref iNum4);
                                 break;
                             case 6:
@@ -65,6 +67,7 @@ namespace CollageCS
                                 AnArry();
                                 break;
                             default:
+                                Console.WriteLine("That's not on the list...\n");
                                 break;
                         }
                         string sSel1;
@@ -74,9 +77,10 @@ namespace CollageCS
                         {
                             Menu = true;
                         }
-                        else if(sSel1 == "n")
+                        else if (sSel1 == "n")
                         {
                             Menu = false;
+                            Console.Clear();
                         }
                         else
                         {
@@ -97,20 +101,21 @@ namespace CollageCS
                                 //Car - 24/03/21
                                 Car();
                                 break;
+                            case 3:
+                                Animation();
+                                break;
                             default:
-                                Console.WriteLine("Something went wrong, try again with a number between 1-2");
+                                Console.WriteLine("Something went wrong, try again with a number between 1-3\n");
                                 break;
                         }
                         break;
-                    case 3:
-                        Animation();
-                        break;
+
                     default:
-                        Console.WriteLine("Hmm, that's not right");
+                        Console.WriteLine("Hmm, that's not right\n");
                         break;
                 }
             }
-            
+
             Console.WriteLine("Press any key to exit: ");
             Console.ReadKey();
         }
@@ -123,10 +128,10 @@ namespace CollageCS
 
             //Student Grades
             byte[] StudentGrades = new byte[5];
-            string[] StudentNames = new string[] {"John", "Bob", "Smith", "David", "Coner"};
+            string[] StudentNames = new string[] { "John", "Bob", "Smith", "David", "Coner" };
             for (int i = 0; i < 5; i++)
             {
-                Console.Write("Please enter the grade for student {0}: ", i+1);
+                Console.Write("Please enter the grade for student {0}: ", i + 1);
                 byte.TryParse(Console.ReadLine(), out StudentGrades[i]);
             }
             Console.WriteLine("\n");
@@ -135,6 +140,7 @@ namespace CollageCS
                 Console.WriteLine("{0} got grade: {1}", StudentNames[j], StudentGrades[j]);
             }
 
+            //Calculating highest grade
             byte HighestGrade = 0;
             byte HighestIndex = 0;
             byte k = 0;
@@ -191,7 +197,7 @@ namespace CollageCS
             double dIntRate = Convert.ToDouble(Console.ReadLine());
             //Amount given by the user
             Console.Write("Please enter your initial amount: £");
-            double dAmount= Convert.ToDouble(Console.ReadLine());
+            double dAmount = Convert.ToDouble(Console.ReadLine());
             //Number of years they will be leaving the money in the bank
             Console.Write("Now enter the number of years you intend to leave it there: ");
             byte bNumOfYears = Convert.ToByte(Console.ReadLine());
@@ -212,7 +218,7 @@ namespace CollageCS
             Console.WriteLine("{0} - {1} = {2}", iNum3, iNum4, iAns2);
         }
 
-        private static void AddTwoNumbers(ref int iNum1,ref int iNum2)
+        private static void AddTwoNumbers(ref int iNum1, ref int iNum2)
         {
             int iAns;
             iNum2 = 60;
@@ -264,11 +270,11 @@ namespace CollageCS
             //    Console.WriteLine("cold");
             //}
 
-            ushort iSal= 0;
+            ushort iSal = 0;
             Console.Write("How much are you earning: £");
             ushort.TryParse(Console.ReadLine(), out iSal);
 
-            
+
             Console.Write("and how long have you worked here: ");
             Byte bYears = Convert.ToByte(Console.ReadLine());
             if (iSal > 40000)
@@ -365,11 +371,6 @@ namespace CollageCS
 
                 }
             }
-        }
-        public static string Truncate(this string value, int maxLength)
-        {
-            if (string.IsNullOrEmpty(value)) return value;
-            return value.Length <= maxLength ? value : value.Substring(0, maxLength);
         }
     }
 }
